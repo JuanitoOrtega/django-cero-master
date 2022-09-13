20d684f338583be098a69da6e0be081271c9f70f
Juanito Ortega <ortegaj83@gmail.com>
2022-09-13 16:23:20 -0400

    templates added

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SQLITE = {
     'default': {
