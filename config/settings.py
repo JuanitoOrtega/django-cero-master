5559aca1c56dfba56737b6ae7832f3ce132c9b29
Juanito Ortega <ortegaj83@gmail.com>
2022-09-19 00:01:39 -0400

    formview para inicio de sesion

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Apps
     'core.erp',
     'core.homepage',
+    'core.login',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = '/erp/category/list'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from core.homepage.views import IndexView
+from core.login.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('erp/', include('core.erp.urls')),
     path('', IndexView.as_view()),
+    # path('login/', LoginFormView.as_view()),
+    path('login/', LoginFormularioView.as_view()),
 ]
